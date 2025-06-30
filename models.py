1c497d49c802e2dd2cf0af79820b33e9b90df857
bautista-johnmatthew <131367336+bautista-johnmatthew@users.noreply.github.com>
2025-06-30 18:37:49 +0800

    Add search dish function (#3)
    * add search_dish function
    
    * add search_ingredient function
    
    * add sample data

--- models.py
+++ models.py
@@ -1,11 +1,11 @@
 import sqlite3
 
-DB_NAME = "pinoy_cuisine.db"
+CUISINE_DB = "pinoy_cuisine.db"
 
 # CREATE TABLES
 def create_tables():
     """Create the necessary tables for the Pinoy Cuisine database."""
-    conn = sqlite3.connect(DB_NAME)
+    conn = sqlite3.connect(CUISINE_DB)
     cur = conn.cursor()
 
     cur.execute("""
@@ -36,10 +36,20 @@ def create_tables():
 
 def insert_default_data():
     """Insert default data into the dishes and ingredients tables."""
-    conn = sqlite3.connect(DB_NAME)
+    conn = sqlite3.connect(CUISINE_DB)
     cur = conn.cursor()
 
     # Insert dishes
+    cur.execute("""INSERT INTO dishes (name, classification, methodology, 
+            origin, taste_profile, description) VALUES (?, ?, ?, ?, ?, ?)""",
+            ("Adobo", "Main Dish", "Stewing", "N/A","Sour", 
+             "Stewed meat marinated in vinegar, soy sauce, and garlic."))
+    
+    latest_id = cur.lastrowid
+    cur.execute(""" INSERT INTO ingredients (dish_id, name, type) VALUES (?, ?, ?)""",
+                (latest_id, "pork", "meat"))
+    cur.execute(""" INSERT INTO ingredients (dish_id, name, type) VALUES (?, ?, ?)""",
+                (latest_id, "garlic", "vegetable"))
     # dict_results
 
     # Dishes data
@@ -49,5 +59,46 @@ def insert_default_data():
 
     print("✅ Default data inserted successfully.")
 
-create_tables()
-insert_default_data()
+def search_dish(id):
+    """ Search for a dish using the ID and return formatted dictionary """
+    conn = sqlite3.connect(CUISINE_DB)
+    cur = conn.cursor()
+    results = cur.execute("SELECT * FROM dishes WHERE id = ?", 
+            (id,)).fetchone()
+    ingredients = cur.execute("SELECT * FROM ingredients WHERE dish_id = ?", 
+            (id,)).fetchall()
+
+    dict_results = {
+    "name" : results[1],
+    "classification" : results[2],
+    "methodology" : results[3],
+    "origin" : results[4],
+    "taste_profile" : results[5],
+    "description" : results[6],
+    "ingredients" : {
+            "meat": [],
+            "vegetable": []
+        }
+    }
+
+    for ingredient in ingredients:
+        if ingredient[3] == "meat":
+            dict_results["ingredients"]["meat"].append(ingredient[2])
+        elif ingredient[3] == "vegetable":
+            dict_results["ingredients"]["vegetable"].append(ingredient[2])
+
+    return dict_results
+
+def search_ingredient(search_ingredient):
+    """ Search for dishes that contain a specific ingredient """
+    conn = sqlite3.connect(CUISINE_DB)
+    cur = conn.cursor()
+    dishes = []
+    query = "SELECT dish_id FROM ingredients WHERE name = LOWER(?)"
+    search_results = cur.execute(query, (search_ingredient,)).fetchall()
+
+    conn.close()
+
+    return search_results
+
+
