836ddae56b5fa75f9c094f0b95a9a72e3efbd693
managbanag-johnmark <jmmanagbanag123@gmail.com>
2025-07-01 10:12:01 +0800

    edit app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template
 from flask_flatpages import FlatPages
 import sqlite3
 from models import CUISINE_DB, search_dish
